7b66f7734ff856325cc525d696f5586912701b74
canaan2000 <canaanhutton@gmail.com>
2025-12-10 13:00:39 -0600

    Ver a0.009
    Door closes now

--- Assets/Scripts/Puzzle Scripts/DoorScript.cs
+++ Assets/Scripts/Puzzle Scripts/DoorScript.cs
@@ -5,10 +5,12 @@ using System.Security.Cryptography.X509Certificates;
 public class DoorScript : MonoBehaviour
 {
     public List<GameObject> puzzleElements;
+
+    public Vector3 initialPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        initialPos = transform.position;
     }
 
     // Update is called once per frame
@@ -29,10 +31,19 @@ public class DoorScript : MonoBehaviour
         {
             OpenDoor();
         }
+        else
+        {
+            CloseDoor();
+        }
     }
 
     void OpenDoor()
     {
-        Destroy(gameObject);
+        transform.position = new Vector3(initialPos.x, initialPos.y + 5.0f, initialPos.z);
+    }
+
+    void CloseDoor()
+    {
+        transform.position = initialPos;
     }
 }
